49795097897c98d4c5a55ea85e0d1f786bd139fe
Geoffrey Holbrook <geof@Geoffreys-MacBook-Pro.local>
2020-03-28 16:03:07 -0400

    Updates

---
+++ precache-manifest.e059405c81b6bcb8238b1de1fc7ad459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89094b8be61d56965a6028ba2b1be27",
+    "url": "/keyboard-plus-code/index.html"
+  },
+  {
+    "revision": "4ce1488ff14c9a5c7041",
+    "url": "/keyboard-plus-code/static/css/main.be255ade.chunk.css"
+  },
+  {
+    "revision": "e820db3c18f3a04f525c",
+    "url": "/keyboard-plus-code/static/js/2.4386ac7b.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/keyboard-plus-code/static/js/2.4386ac7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce1488ff14c9a5c7041",
+    "url": "/keyboard-plus-code/static/js/main.a761c217.chunk.js"
+  },
+  {
+    "revision": "2a95487b42f64eb5957f",
+    "url": "/keyboard-plus-code/static/js/runtime-main.1fcedac4.js"
+  }
+]);
